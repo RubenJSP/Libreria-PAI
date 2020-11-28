2a6ab665961f60fbee83c5ae63d4c3ab48ee8fd8
jenilo <jennylopezu@gmail.com>
2020-11-28 13:54:19 -0700

    Menu y correcciones
    Links activos, menu responsivo y correciones minimas

--- routes/web.php
+++ routes/web.php
@@ -23,24 +23,22 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 
 Route::group(['middleware' => 'auth'], function () {
     //Book routes
-    Route::get('/books','BookController@index')->name('books');;
-    Route::get('/books','BookController@index');
+    Route::get('/books','BookController@index')->name('books');
     Route::post('/books', 'BookController@store');
     Route::put('/books', 'BookController@update');
     Route::delete('/books/{book}', 'BookController@destroy');
     //Loan routes
     Route::get('/loan','LoanController@index')->name('loans');
-    Route::post('/loan', 'LoanController@store');
     Route::put('/loan', 'LoanController@update');
     Route::get('/data','LoanController@data');
     Route::delete('/loan/{loan}', 'LoanController@destroy');
     //Categories routes
-    Route::get('/categories', 'CategoryController@index')->name('categories');; 
+    Route::get('/categories', 'CategoryController@index')->name('categories');
     Route::post('/categories','CategoryController@store');
     Route::put('/categories','CategoryController@update');
     Route::delete('/categories/{category}','CategoryController@destroy');
     //User routes
-    Route::get('/users','UserController@index')->name('users');;
+    Route::get('/users','UserController@index')->name('users');
     Route::post('/users','UserController@store');
 
 });
